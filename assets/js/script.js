b67ddd12d7b301c92246f80f8acf102599d5bbb3
mandeepkaur86 <mandeepkaurdia@gmail.com>
2023-03-08 13:34:24 -0800

    pannyflip

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,26 +2,52 @@
 
 
 document.addEventListener('DOMContentLoaded', function () {
-    // This is just a sanity check to make sure your JavaScript script is getting loaded
-    // You can remove it once you see it in your browser console in the developer tools
-    console.log('Hi')
-
-    // TODO: Add event listener and handler for flip and clear buttons
-
-    // Flip Button Click Handler
-        // TODO: Determine flip outcome
-        // TODO: Update image and status message in the DOM
-
-        // Update the scorboard
-        // TODO: Calculate the total number of rolls/flips
-        // Make variables to track the percentages of heads and tails
-        // TODO: Use the calculated total to calculate the percentages
-        // HINT: Make sure not to divide by 0! (if total is 0, percent will be 0 as well)
-        // TODO: Update the display of each table cell
+    let heads = 0
+     let headsPercentage  = 0
+      let tails = 0
+         let tailsPercentage = 0
+         let totalflip = 0
 
 
-    // Clear Button Click Handler
-        // TODO: Reset global variables to 0
-        // TODO: Update the scoreboard (same logic as in flip button click handler)
+         document.querySelector('#fiip').addEventListener('click', function {
+            if (Math.random() > .5){
+                heads++
+                total++
+                headsPercentage = (Math.round(heads/total * 100))
+                if (tails > 0) {
+                    tailsPercent = (Math.round(tails/total * 100))
+
+                } 
+                document.querySelector(`#penny-image`).src = 'assests/images/penny-heads.jpg'
+                document.querySelector(`#message`).innerHTML = 'you flipped heads!'
+                document.querySelector(`#heads`).innerHTML = heads
+                document.querySelector(`#heads-percent`).innerHTML = `${headsPercent}%`
+                document.querySelector(`#tails-percent`).innerHTML = `${tailsPercent}%`
+            } else {
+                tails++
+                total++
+                tailsPercent = (Math.round(tails/total * 100))
+                if(head > 0){
+                    headsPercent = (Math.round(head/total * 100))
+                }
+                document.querySelector(`#tails-image`).src = 'assets/images/penny-tails.jpg'
+                document.querySelector(`#message`).innerHTML = 'you flipped tails'
+                ocument.querySelector(`#heads`).innerHTML = heads
+                document.querySelector(`#heads-percent`).innerHTML = `${headsPercent}%`
+                document.querySelector(`#tails-percent`).innerHTML = `${tailsPercent}%`
+ }
+
+         }
+
+         
+
+
+    
+
+    
+
+
+
+    
 
 })
